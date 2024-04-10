0c71121f11457919a412fe3853137318e6e0b16e
TheJuicedOrange <powerhousegames206@gmail.com>
2024-04-10 13:38:53 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Noah Isaac
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Dr. Johnson! This is Noah Isaac.");
+
+	}
+
+}
